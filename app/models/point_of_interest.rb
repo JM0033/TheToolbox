08dc6df2f439878d2aeb4d21bd51e658ee47f08e
Jeremy M <jm0033@tutamail.com>
2022-08-29 17:33:40 +0200

    Model and table created

---
+++ app/models/point_of_interest.rb
@@ -0,0 +1,2 @@
+class PointOfInterest < ApplicationRecord
+end
---
+++ test/models/point_of_interest_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PointOfInterestTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
