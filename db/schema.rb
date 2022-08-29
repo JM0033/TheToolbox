2625120cb464380fa319fabbf5159ce077910ceb
Jeremy M <jm0033@tutamail.com>
2022-08-29 17:26:20 +0200

    Devise fully set

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_151109) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_29_151521) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_151109) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "description"
+    t.string "biker_status"
+    t.string "bike_model"
+    t.string "language"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
