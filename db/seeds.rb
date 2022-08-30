0aff9b2afe36f1de18a554e54d76e7ce6a610cd7
Jeremy M <jm0033@tutamail.com>
2022-08-30 15:16:03 +0200

    Added Destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Destroying old POIs..."
+Answer.destroy_all
+puts "Destroying old POIs completed."
 
 puts "Creating Users..."
 @clemence = User.new(
