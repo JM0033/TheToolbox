dec8fe981b3f262b2208826c8dae017dd27c7595
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2022-08-31 12:11:10 +0200

    Auto center de la carte sur l'ip du user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
   def home
-    @ip = user_ip
+    @ip = "152.89.163.228"
     results = Geocoder.search(@ip)
     results.first.coordinates
+    @userLat = results.first.coordinates[0]
+    @userLgn = results.first.coordinates[1]
   end
 
   private
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,27 +1,35 @@
 import { Controller } from "@hotwired/stimulus"
-import mapboxgl from "mapbox-gl"
 
 export default class extends Controller {
   static values = {
     apiKey: String,
-    markers: Array
+    markers: Array,
+    userPosition: Array
   }
 
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
-    console.log("connected to map")
 
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        coordinates = [position.coords.latitude, position.coords.longitude];
-        console.log(coordinates)
-        const map = new mapboxgl.Map({
-          container: "map",
-          style: "mapbox://styles/mapbox/streets-v9",
-          center: [ position.coords.latitude, position.coords.longitude ],
-          zoom: 12
-        })
-      });
-    }
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v10",
+      center: [this.userPositionValue[0], this.userPositionValue[1]],
+      zoom: 14
+    })
+    this.#addUserToMap()
+    // this.#fitMapToUser()
   }
-}
+
+  #addUserToMap() {
+      new mapboxgl.Marker()
+        .setLngLat([ this.userPositionValue[0], this.userPositionValue[1] ])
+        .addTo(this.map)
+      console.log(this.userPositionValue)
+    }
+
+  // #fitMapToUser() {
+  //   const bounds = new mapboxgl.LngLatBounds()
+  //   this.userPosition.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+  //   this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  // }
+  }
