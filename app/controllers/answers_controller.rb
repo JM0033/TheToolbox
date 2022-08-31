273077ce1af99b0da10979e4ccd4520c7dd3f960
Clemence Parodi <clemparodi1609@hotmail.com>
2022-08-31 18:23:49 +0200

    added answer form to question show page

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,10 +1,12 @@
 class AnswersController < ApplicationController
   def create
+    @question = Question.find(params[:question_id])
     @answer = Answer.new(answer_params)
-    @question = @answer.question(:question_id)
+    @answer.question = @question
+
     if @answer.save!
       flash[:notice] = "Your answer has successfully been submitted"
-      redirect_to questions_path
+      redirect_to question_path(@question)
     else
       render :new, status: :unprocessable_entity
     end
